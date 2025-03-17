5db8cd8b522d5b3b72e7bc4f8aeddad9f215ae69
QcrTiMo <3192377376@qq.com>
2025-03-17 21:33:06 +0800

    Add files via upload

---
+++ src/js/hello.js
@@ -0,0 +1,21 @@
+import chatLogo from '/pictures/aichat.png';
+import { setupCounter } from '../utils/counter.js'; // 注意路径
+
+export function showHelloPage(userName) {
+    document.querySelector('#app').innerHTML = `
+    <div>
+      <a href="https://eridanus-doc.netlify.app/" target="_blank">
+        <img src="${chatLogo}" class="logo" alt="Vite logo" />
+      </a>
+      <h1>欢迎，${userName}!</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        基于Java+vite+JavaScript的AI聊天网页程序
+      </p>
+    </div>
+  `;
+
+    setupCounter(document.querySelector('#counter'));
+}
